828c236ef067ba34059c1eb9b031703d353ba059
KARTAVYA SONI <96534227+Kartavyasoni@users.noreply.github.com>
2024-03-28 22:58:52 +0530

    Create 442_Find_All_Duplicates_in_an_Array.py

---
+++ 442_Find_All_Duplicates_in_an_Array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        seen = set()
+        op = list()
+
+        for i in nums:
+            if i in seen:
+                op.append(i)
+            else:
+                seen.add(i)
+        return op
